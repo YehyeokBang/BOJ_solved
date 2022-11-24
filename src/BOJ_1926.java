ac4bcc6110ca207474decabe9192a523edfccff9
YehyeokBang <qkddpgur318@gmail.com>
2022-11-24 09:24:55 +0900

    Add BOJ_1926

--- src/BOJ_1926.java
+++ src/BOJ_1926.java
@@ -7,6 +7,8 @@ public class BOJ_1926 {
     static int m; // 도화지의 가로 크기
     static int[][] paper; // 도화지
     static boolean[][] visited; // 탐색 시 방문 여부
+    static int size = 0; // 크기 측정 용도
+    static int maxSize = 0; // 가장 넓은 도형의 크기
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,40 +32,52 @@ public class BOJ_1926 {
 
         visited = new boolean[n][m];
 
-
         int countPainting = 0; // 그림 개수
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(paper[i][j] == 1 && !visited[i][j]) {
                     dfs(i, j);
                     countPainting++;
+
+                    // 최대 그림의 크기 저장
+                    if(size > maxSize) {
+                        maxSize = size;
+                    }
+                    size = 0; // 각 그림마다 사이즈를 측정해야 하기에 0으로 초기화
                 }
             }
         }
-
-
-
+        // 출력
+        sb.append(countPainting).append("\n");
+        sb.append(maxSize);
+        bw.write(String.valueOf(sb));
+        bw.close();
     }
 
     static void dfs(int row, int col) {
-        visited[row][col] = true;
+        visited[row][col] = true; // 방문 체크
+        size++; // 사이즈 측정
 
+        // 상
         if(row > 0) {
             if(paper[row - 1][col] == 1 && !visited[row - 1][col]) {
                 dfs(row - 1, col);
             }
         }
-        if(col > 0) {
-            if(paper[row][col - 1] == 1 && !visited[row][col - 1]) {
-                dfs(row, col - 1);
-            }
-        }
+        // 하
         if(row < n - 1) {
             if(paper[row + 1][col] == 1 && !visited[row + 1][col]) {
                 dfs(row + 1, col);
             }
         }
-        if(col < n - 1) {
+        // 좌
+        if(col > 0) {
+            if(paper[row][col - 1] == 1 && !visited[row][col - 1]) {
+                dfs(row, col - 1);
+            }
+        }
+        // 우
+        if(col < m - 1) {
             if(paper[row][col + 1] == 1 && !visited[row][col + 1]) {
                 dfs(row, col + 1);
             }
