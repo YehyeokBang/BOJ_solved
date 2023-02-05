539a5cd782b4162cf7f8176a1026b7e1567eefe2
YehyeokBang <qkddpgur318@gmail.com>
2023-02-05 22:53:38 +0900

    Add BOJ_5337

---
+++ src/BOJ_5337.java
@@ -0,0 +1,10 @@
+// 웰컴
+public class BOJ_5337 {
+    public static void main(String[] args) {
+        System.out.print(
+                ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/."
+        );
+    }
+}
