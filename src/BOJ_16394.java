f729ffa5d2cbe1f46695d7c285f175fe7edc8780
YehyeokBang <qkddpgur318@gmail.com>
2023-02-09 12:32:00 +0900

    Add BOJ_16394

---
+++ src/BOJ_16394.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// 홍익대학교
+public class BOJ_16394 {
+    public static void main(String[] args) {
+        // 특정 년도 입력
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        // 출력
+        System.out.println(y - 1946);
+    }
+}
